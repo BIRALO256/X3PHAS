568c4f0f5448582f252a6f4b3ebaf5b5b8fa5fa6
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-02-02 22:46:19 +0300

    nav bar

---
+++ src/assets/data.js
@@ -0,0 +1,20 @@
+export const socials =[
+    {
+        logo:'<box-icon type='logo' name='youtube'></box-icon>'
+},
+    
+{
+        logo:'<box-icon type='logo' name='linkedin-square'></box-icon>'
+},
+
+{
+    logo:'<box-icon type='logo' name='linkedin-square'></box-icon>'
+},
+
+{
+    logo:'<box-icon name='facebook' type='logo' ></box-icon>'
+},
+{
+    logo:'<box-icon name='github' type='logo' ></box-icon>'
+},
+]
