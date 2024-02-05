596373373b747d9a77b6b19269076d50e2329860
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-02-05 23:00:29 +0300

    improved navbar

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,20 +1,16 @@
 export const socials =[
     {
-        logo:'<box-icon type='logo' name='youtube'></box-icon>'
+        logo:'bx bxl-youtube'
 },
     
 {
-        logo:'<box-icon type='logo' name='linkedin-square'></box-icon>'
+        logo:'bx  bxl-linkedin-square'
 },
 
 {
-    logo:'<box-icon type='logo' name='linkedin-square'></box-icon>'
-},
-
-{
-    logo:'<box-icon name='facebook' type='logo' ></box-icon>'
+    logo:'bx bxl-facebook'
 },
 {
-    logo:'<box-icon name='github' type='logo' ></box-icon>'
+    logo:'bx bxl-github'
 },
 ]
